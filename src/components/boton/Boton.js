8f859a62fa7a337164574ed0fa28338d2a498027
Gerardo Cid <gcid@skillz.com>
2022-07-06 13:40:09 -0700

    Receiving tokens from NPM

--- src/components/boton/Boton.js
+++ src/components/boton/Boton.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Button, Icon } from "@material-ui/core";
+import { Button} from "@material-ui/core";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import { makeStyles } from "@material-ui/core/styles";
+import {PrimaryMain, PrimaryBorder} from "@skillzet/tokens";
 
 export const Boton = (props) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -10,8 +11,9 @@ export const Boton = (props) => {
   const customStyle = useStyles();
   const customStyle2 = useStyles2();
 
-  console.log(customStyle);
-  console.log(customStyle2);
+  //console.log("NPM COLORS -> ",Colors);
+  console.log("NPM ActionMain -> ",PrimaryMain);
+ 
 
   const {
     onClick,
@@ -83,9 +85,9 @@ const useStyles2 = makeStyles(({ palette, ...theme }) => console.log(palette));
 const useStyles = makeStyles(({ palette, ...theme }) => ({
   buttonStyle: {
     //color:'#FF0000',
-    border: `1px solid ${palette.primary.main}`,
+    border: `1px solid ${PrimaryBorder}`,
     //boxShadow: theme.shadows[8],
-    //color: palette.text.primary,
+    color: PrimaryMain,
     "&:hover": {
       width: "var(--sidenav-width)",
       "& .sidenavHoverShow": {
